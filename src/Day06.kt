4bfd0ed91a4d6e33e576ed68d9a678b087a04c49
DuroCodes <davidwright13503@gmail.com>
2023-12-06 09:19:38 -0500

    feat: day 6 easy

---
+++ src/Day06.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val d = Day06(readInput("input"))
+    println(d.solvePart1())
+    println(d.solvePart2())
+}
+
+class Day06(private val input: List<String>) {
+    fun solvePart1() = solve(input)
+
+    fun solvePart2() = solve(input.map{ it.replace(" ", "") })
+
+    private fun solve(input: List<String>): Long {
+        val (times, distances) = input.map {
+            it.substringAfter(":").split(' ').filter { s -> s.isNotBlank() }.map { s -> s.toLong() }
+        }
+
+       return times.zip(distances)
+            .map { (time, distance) -> (0..time).map { it * (time - it) }.count { it > distance }.toLong() }
+            .reduce(Long::times)
+    }
+}
