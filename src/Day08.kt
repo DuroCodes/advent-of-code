490876261e8e21c2bfdc0ce123bfc500c9721f7f
DuroCodes <davidwright13503@gmail.com>
2023-12-08 09:29:26 -0500

    feat: day 8

---
+++ src/Day08.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val d = Day08(readInput("input"))
+    println(d.solvePart1())
+    println(d.solvePart2())
+}
+
+class Day08(input: List<String>) {
+    private val instructions = input.first()
+    private val network = input.drop(2).associate { l ->
+        l.split(" = ").let {
+            val (node, targets) = it.first() to it.last().drop(1).dropLast(1).split(", ")
+            node to mapOf('L' to targets.first(), 'R' to targets.last())
+        }
+    }
+
+    private fun solveForNode(str: String): Long {
+        var cur = str
+        var i = 0
+        while (!cur.endsWith('Z')) {
+            val inst = instructions[i++ % instructions.length]
+            cur = network[cur]!![inst]!!
+        }
+        return i.toLong()
+    }
+
+    fun solvePart1() = solveForNode(network.keys.find { it == "AAA" }!!)
+    fun solvePart2() = network.keys.filter { it.last() == 'A' }.map { solveForNode(it) }.reduce(::lcm)
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -4,7 +4,7 @@ import kotlin.io.path.readLines
 fun readInput(name: String) = Path("src/$name.txt").readLines()
 data class Vector2D(val x: Int, val y: Int)
 
-fun <T>time(block: () -> T) {
+fun <T> time(block: () -> T) {
     val start = System.currentTimeMillis()
     try {
         block.invoke()
@@ -12,4 +12,21 @@ fun <T>time(block: () -> T) {
         val end = System.currentTimeMillis()
         println("Took ${end - start} ms.")
     }
+}
+
+fun lcm(a: Long, b: Long) = a * b / gcd(a, b)
+
+fun gcd(first: Long, second: Long): Long {
+    var a = first
+    var b = second
+
+    while (a > 0 && b > 0) {
+        if (a >= b) {
+            a %= b
+        } else {
+            b %= a
+        }
+    }
+
+    return maxOf(a, b)
 }
